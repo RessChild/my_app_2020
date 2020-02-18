2185b061d22f9c20cb599fe78e40e20722d3f0d2
RessChild <39401832+RessChild@users.noreply.github.com>
2020-02-18 18:07:49 +0900

    연습용 Food

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 Movie App 2020
 
 React JS Funtamentals Course
+
+# React 환경설정
+- npm, npx 모두 설치
+- 원하는 디렉토리로 이동
+- npx create-react-app 생성할_폴더명
+
+# 깃 연동 방법
+0. git init
+1. git remote add origin 연동시킬_URL
+2. git add .
+3. git commit -m "원하는 내용"
+4. git push -u origin master
--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Proptypes from"prop-types";
+
+const foodILike = [
+  {
+    id:1,
+    name:"kimchi",
+    pic:"https://pds.joins.com/news/component/htmlphoto_mmdata/201910/01/htm_20191001205346742941.jpg",
+    rating:5,
+  },{
+    id:2,
+    name:"ramen",
+    pic:"https://pds.joins.com/news/component/htmlphoto_mmdata/201910/01/htm_20191001205346742941.jpg",
+    rating:4.8,
+  },{
+    id:3,
+    name:"삼겹살",
+    pic:"https://pds.joins.com/news/component/htmlphoto_mmdata/201910/01/htm_20191001205346742941.jpg",
+    rating:3.6,
+  }]
+
+function renderFood(dish){
+  return <Food
+           key={dish.id} 
+           favorite={dish.name} 
+           pic={dish.pic} 
+           rating={dish.rating}
+          />
+}
+
+  // 함수를 따로 짜서 선언해도 상관없고\
+  // 람다형식으로 짜도 OK
+  // 입력명 => ( 함수 내용 )
+
+// component 호출 함수는 무조건 대문자로 시작해야함
+function Food( { favorite, pic, rating } ){ // props.fav 랑 동일
+  console.log({rating})
+  return (
+    <div>
+      <h1>i like {favorite} </h1>
+      <h4>{rating}/5.0 </h4>
+      <img src={pic} alt={favorite}/>
+    </div>
+  );
+}
+
+Food.propTypes = {
+  name:Proptypes.string.isRequired,
+  picture: Proptypes.string.isRequired,
+  rating: Proptypes.number
+}; // 인자전달 오류찾는 방법
+// isRequired 가 없으면 필수는 아님 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {foodILike.map(renderFood)}
     </div>
   );
 }
