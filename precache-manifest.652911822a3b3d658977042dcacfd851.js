046d2d50439e9d17bc6fefdc9f4b863aa52c3cd4
RessChild <39401832+RessChild@users.noreply.github.com>
2020-02-21 17:12:47 +0900

    Updates

---
+++ precache-manifest.652911822a3b3d658977042dcacfd851.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c1bdb8a94d600b432f69ba1b5a6742",
+    "url": "/my_app_2020/index.html"
+  },
+  {
+    "revision": "dff2d60ee4802b9372c0",
+    "url": "/my_app_2020/static/css/main.12a45380.chunk.css"
+  },
+  {
+    "revision": "98a3732eeb84de380d1f",
+    "url": "/my_app_2020/static/js/2.c5055275.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my_app_2020/static/js/2.c5055275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dff2d60ee4802b9372c0",
+    "url": "/my_app_2020/static/js/main.e4f60210.chunk.js"
+  },
+  {
+    "revision": "93009ddeacce37bd78ef",
+    "url": "/my_app_2020/static/js/runtime-main.571b15a3.js"
+  }
+]);
